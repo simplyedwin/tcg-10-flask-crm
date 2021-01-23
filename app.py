fddb235ee0cb7eca8cbd5d470311a530ec98a6f1
simplyedwin <simplyedwin@gmail.com>
2021-01-23 03:22:43 +0000

    init

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import json
+
+app = Flask(__name__)
+database = {}
+with open('customers.json') as file:
+    database = json.load(file)
+print(database)
+
+@app.route('/')
+def home():
+    return render_template('home.template.html')
+
+
+@app.route('/customer')
+def about():
+    return render_template('customer.template.html',db=database)
+
+
+@app.route('/lucky')
+def luckyno():
+    num = random.randint(1000, 9999)
+    return 'Your lucky number is {}'.format(num)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get('IP'), port=int(
+        os.environ.get('PORT')), debug=True)
+    # app.run(host='localhost', port=8080, debug=True)
